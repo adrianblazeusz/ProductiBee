a6db090dd8be03f2a8d3acaa6f656dc40e451aad
Adrian Błazeusz <117313800+adrianblazeusz@users.noreply.github.com>
2023-07-28 17:18:42 +0200

    Add files via upload

--- new_f.py
+++ new_f.py
@@ -3,51 +3,111 @@ import tkinter.messagebox
 import customtkinter
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        # configure window
         self.title("ProductiBee")
-        self.geometry(f"{800}x{400}")
+        self.geometry("700x450")
 
-        # configure grid layout (4x4)
+        # set grid layout 1x2
+        self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure((2, 3), weight=0)
-        self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        # create sidebar frame with widgets
-        self.sidebar_frame = customtkinter.CTkFrame(self, width=120, corner_radius=0)
-        self.sidebar_frame.grid(row=0, column=0, rowspan=1, sticky="nsew", padx=(0, 5), pady=(0, 5))
-        self.sidebar_frame.grid_rowconfigure(4, weight=1)
-        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="ProductiBee", font=customtkinter.CTkFont(size=25, weight="bold"))
-        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, text="Work Mode", command=self.sidebar_button_event)
-        self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Analysis", command=self.sidebar_button_event)
-        self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
-        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, text="Blocker", command=self.sidebar_button_event)
-        self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
-        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
-        self.appearance_mode_label.grid(row=5, column=0, padx=5, pady=(5, 0))
-        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
-                                                                       command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+       
+        # create navigation frame
+        self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
+        self.navigation_frame.grid_rowconfigure(4, weight=1)
 
-        # set default values
-        self.appearance_mode_optionemenu.set("Dark")
-        self.tabview = customtkinter.CTkTabview(self, width=600, height=400)
-        self.tabview.grid(row=0, column=1, rowspan=1, padx=(5, 5), pady=(0, 10), sticky="nsew")
+        # create navigation frame label
+        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="  ProductiBee",
+                                                             compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
+        self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
+
+        # create home button 
+        self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Home",
+                                                   fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                   anchor="w", command=self.frame_home_button_event)
+        self.home_button.grid(row=1, column=0, sticky="ew")
+
+        # create work mode button
+        self.work_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Work Mode",
+                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                      anchor="w", command=self.frame_work_button_event)
+        self.work_button.grid(row=2, column=0, sticky="ew")
+
+        # create blocker button
+        self.blocker_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Blocker",
+                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                      anchor="w", command=self.frame_blocker_button_event)
+        self.blocker_button.grid(row=3, column=0, sticky="ew")
+
+        # create appearance mode menu
+        self.appearance_mode_menu = customtkinter.CTkOptionMenu(self.navigation_frame, values=["Dark", "Light",  "System"],
+                                                                command=self.change_appearance_mode_event)
+        self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
+
+        # create home frame
+        self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.home_frame.grid_columnconfigure(0, weight=1)
+
+        self.home_frame_large_image_label = customtkinter.CTkLabel(self.home_frame, text="Logo ProductiBee")
+        self.home_frame_large_image_label.grid(row=0, column=0, padx=20, pady=10)
+
+        self.home_frame_button_1 = customtkinter.CTkButton(self.home_frame, text="HI")
+        self.home_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
+       
         
 
-    def change_appearance_mode_event(self, new_appearance_mode: str):
+        # create second frame
+        self.work_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.work_frame.grid_columnconfigure(0, weight=1)
+
+        self.home_frame_button_2 = customtkinter.CTkButton(self.work_frame, text="Work", compound="right")
+        self.home_frame_button_2.grid(row=2, column=0, padx=20, pady=10)
+
+
+
+        # create third frame
+        self.blocker_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.blocker_frame.grid_columnconfigure(0, weight=1)
+
+        self.home_frame_button_3 = customtkinter.CTkButton(self.blocker_frame, text="Blocker", compound="top")
+        self.home_frame_button_3.grid(row=3, column=0, padx=20, pady=10)
+
+
+
+        # select default frame
+        self.select_frame_by_name("home")
+
+    def select_frame_by_name(self, name):
+        # Hide all frames first using grid_remove() instead of grid_forget()
+        self.home_frame.grid_remove()
+        self.work_frame.grid_remove()
+        self.blocker_frame.grid_remove()
+
+        # Show the selected frame based on the given name
+        if name == "home":
+            self.home_frame.grid(row=0, column=1, sticky="nsew")
+        elif name == "work":
+            self.work_frame.grid(row=0, column=1, sticky="nsew")
+        elif name == "blocker":
+            self.blocker_frame.grid(row=0, column=1, sticky="nsew")
+
+    def frame_home_button_event(self):
+        self.select_frame_by_name("home")
+
+    def frame_work_button_event(self):
+        self.select_frame_by_name("work")
+
+    def frame_blocker_button_event(self):
+        self.select_frame_by_name("blocker")
+
+    def change_appearance_mode_event(self, new_appearance_mode):
         customtkinter.set_appearance_mode(new_appearance_mode)
-
-
-    def sidebar_button_event(self):
-        print("sidebar_button click")
 
 
 if __name__ == "__main__":
