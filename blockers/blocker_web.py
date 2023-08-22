c689f537b16e37d3adb7936b8770d29724b8ee19
adrianblazeusz <adrian.blazeusz@gmail.com>
2023-08-22 13:34:00 +0200

    working-web

--- blockers/blocker_web.py
+++ blockers/blocker_web.py
@@ -2,38 +2,63 @@ from func.json_manager import JSONManager
 
 
 class Web_blocker:
-    def __init__(self):
-        self.site_to_kill = set()
-        self.hosts_path = r'C:\Windows\System32\drivers\etc\hosts'
-        self.redirect = "127.0.0.1"
-        self.state_file = "log/process_killer_state.json"
-        self.json_m = JSONManager(self.state_file)
 
+    def __init__(self):
+        self.hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+        self.redirect = "127.0.0.1"
+        self.site_to_kill = set()
+        self.common_subdomains = {
+            "facebook.com": ["en-us", "pl-pl", "es-la", "fr-fr", "de-de", "it-it", "ar-ar", "pt-br"]
+        }
+
+    def is_website_blocked(self, website):
+        """
+        Checks if a website is already blocked in the hosts file.
+        """
+        with open(self.hosts_path, "r") as file:
+            content = file.read()
+            return website in content
 
     def block_websites(self):
-        print("Block sites")
-        with open(self.hosts_path, 'r+') as hostfile:
-            hosts_content = hostfile.read()
+        """
+        Block the websites by adding them to the hosts file.
+        """
+        with open(self.hosts_path, "r+") as file:
+            content = file.read()
             for site in self.site_to_kill:
-                if site not in hosts_content:
-                    hostfile.write(self.redirect + ' ' + site + '\n')
-            
+                www_site = "www." + site
+
+                # Block the main domain and www variant
+                if site not in content:
+                    file.write(self.redirect + " " + site + "\n")
+                if www_site not in content:
+                    file.write(self.redirect + " " + www_site + "\n")
+                
+                # Block common subdomains if they exist in our dictionary
+                subdomains = self.common_subdomains.get(site, [])
+                for subdomain in subdomains:
+                    full_subdomain = subdomain + "." + site
+                    if full_subdomain not in content:
+                        file.write(self.redirect + " " + full_subdomain + "\n")
 
     def unblock_websites(self):
-        print('Unblock sites')
-        with open(self.hosts_path, 'r+') as hostfile:
-            lines = hostfile.readlines()
-            hostfile.seek(0)
+        """
+        Unblock the websites by removing them from the hosts file.
+        """
+        with open(self.hosts_path, "r") as file:
+            lines = file.readlines()
+        
+        with open(self.hosts_path, "w") as file:
             for line in lines:
                 if not any(site in line for site in self.site_to_kill):
-                    hostfile.write(line)
-            hostfile.truncate()
-    
-    def set_blocked_websites(self, websites, add_new=True):
-        if add_new:
-            self.site_to_kill.update(websites)  
-        else:
-            self.site_to_kill = set(websites)
+                    file.write(line)
+
+    def set_blocked_websites(self, websites_list):
+        for website in websites_list:
+            if "www." in website:
+                self.site_to_kill.add(website.replace("www.", ""))
+            else:
+                self.site_to_kill.add(website)
 
 if __name__ == "__main__":
 
